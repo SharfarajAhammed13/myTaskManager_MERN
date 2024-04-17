d2b34dccd8217be793db4f45660962f18fa5029d
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-04-17 21:00:43 +0530

    Created Routes in App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Task from './pages/Task'
+
 export default function App() {
   return (
-    <div className=" text-3xl text-red-700">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sign-in' element={<SignIn />}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard />}/>
+        <Route path='/task' element={<Task/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
