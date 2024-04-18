b4c38812109e454477944ce933a878698bfc36fe
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-04-18 14:59:20 +0530

    admin logic added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default:
           'https://static-00.iconduck.com/assets.00/profile-circle-icon-2048x2048-cqe5466q.png',
+      },
+      isAdmin: {
+        type: Boolean,
+        default: false,
       }, 
     },
     {timestamps: true}
