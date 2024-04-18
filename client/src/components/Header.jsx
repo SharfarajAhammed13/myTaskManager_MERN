909ae866607f34e6e4f6b55c570f59046145ffc6
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-04-18 08:19:43 +0530

    Header done, Started Server and Connected to DB

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+export default function Header() {
+    const path = useLocation().pathname;
+  return (
+    <Navbar className='border-b-2'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-red-500 via-blue-600 to-blue-400 rounded-lg text-white'>
+          My Task Manager
+        </span>
+      </Link>
+      <form>
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Link to='/sign-in'>
+          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/tasks"} as={'div'}>
+          <Link to='/tasks'>My Tasks</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
