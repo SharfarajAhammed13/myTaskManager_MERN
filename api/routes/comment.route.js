ce7a7473aa0fba86959003f34da92fc70c217eb5
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-04-19 17:16:54 +0530

    add delete and edit function

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,7 +3,9 @@ import { verifyToken } from '../utils/verifyUser.js';
 import { 
     createComment ,
     getTaskComments,
-    likeComment
+    likeComment,
+    editComment,
+    deleteComment
     } from '../controllers/comment.controller.js';
 
 const router = express.Router();
@@ -12,6 +14,7 @@ const router = express.Router();
 router.post('/create', verifyToken, createComment);
 router.get('/getTaskComments/:taskId', getTaskComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
-
+router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 export default router;
