ad93f0f40bf7f9ea18f46fbf898f37d35d9614c6
SHAIK SHARFARAJ AHAMMED <sharfaraj13@gmail.com>
2024-04-19 17:04:01 +0530

    added like s

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,13 +2,16 @@ import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import { 
     createComment ,
-    getPostComments,
+    getTaskComments,
+    likeComment
     } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken, createComment);
-router.get('/getTaskComment/:taskId', getPostComments);
+router.get('/getTaskComments/:taskId', getTaskComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+
 
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,16 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import About from "./pages/About";
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
-import CreateTask from './pages/CreateTask';
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreateTask from "./pages/CreateTask";
 import UpdateTask from "./pages/UpdateTask";
 import TaskPage from "./pages/TaskPage";
+import ScrollToTop from "./components/ScrollTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -22,15 +24,14 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          
         </Route>
         {/* <Route path="/task" element={<Task />} /> */}
         <Route element={<OnlyAdminPrivateRoute />}>
-          <Route path='/create-task' element={<CreateTask />} />
-          <Route path='/update-task/:taskId' element={<UpdateTask />} />
+          <Route path="/create-task" element={<CreateTask />} />
+          <Route path="/update-task/:taskId" element={<UpdateTask />} />
         </Route>
-        <Route path="/about" element={<About />}/>
-        <Route path='/task/:taskSlug' element={<TaskPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/task/:taskSlug" element={<TaskPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
